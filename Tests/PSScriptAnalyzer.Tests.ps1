7c912ff8680e4e8d4ba07f38940083c7036cfd8c
beatcracker <beatcracker@users.noreply.github.com>
2016-05-13 15:32:04 +0300

    Added PSScriptAnalyzer tests

---
+++ Tests/PSScriptAnalyzer.Tests.ps1
@@ -0,0 +1,16 @@
+$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$FunctionName = [System.IO.Path]::GetFileNameWithoutExtension($MyInvocation.MyCommand.Name).Replace('.Tests', '')
+
+Describe "$FunctionName" {
+    It 'Should not have any PSScriptAnalyzer warnings' {
+        $ScriptWarnings = @(
+            Get-ChildItem -LiteralPath $here | Where-Object {
+                # Excluding temp folder, tests, and third-party function
+                $_.FullName -notmatch '.+\\\.wip.*' -and 
+                $_.FullName -notmatch '.+\\Tests\.*' -and
+                $_.Name -ne 'Invoke-Sqlcmd2.ps1'
+            } | Invoke-ScriptAnalyzer
+        )
+        $ScriptWarnings.Length | Should be 0
+    }
+}
